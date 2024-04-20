c759bfc60a185c18d789b23e8400790f01106c43
cnvinay <vinaycn46@gmail.com>
2024-04-20 16:57:30 +0530

    ghch

--- jenkinesproject/src/test/java/myFirstProject/ElementPresentOrNotTest.java
+++ jenkinesproject/src/test/java/myFirstProject/ElementPresentOrNotTest.java
@@ -44,35 +44,35 @@ public class ElementPresentOrNotTest {
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		WebElement phone = driver.findElement(By.id("select1"));
-		Select s= new Select(phone);
-		s.selectByValue("+91");
-		driver.findElement(By.id("phone")).sendKeys("9964023912");
-		WebElement gender = driver.findElement(By.id("select2"));
-		Select s1= new Select(gender);
-		s1.selectByIndex(0);
-		WebElement country = driver.findElement(By.id("select3"));
-		Select s2= new Select(country);
-		List<WebElement> options = s2.getOptions();
-		int count= options.size();
-		int size=0;
-		String element = "india";
-		for(int i=1; i<count; i++)
-		{
-			String text= options.get(i).getText();
-			if(text.equalsIgnoreCase(element)) {
-				//System.out.println(element+ " : is present");
-				size++;
-				break;
-			}
-		}
-		if(size==1)
-		{
-			Reporter.log(element+ " : present", true);
-		}
-		else {
-			Reporter.log(element+ " : not present", true);
-		}
+//		WebElement phone = driver.findElement(By.id("select1"));
+//		Select s= new Select(phone);
+//		s.selectByValue("+91");
+//		driver.findElement(By.id("phone")).sendKeys("9964023912");
+//		WebElement gender = driver.findElement(By.id("select2"));
+//		Select s1= new Select(gender);
+//		s1.selectByIndex(0);
+//		WebElement country = driver.findElement(By.id("select3"));
+//		Select s2= new Select(country);
+//		List<WebElement> options = s2.getOptions();
+//		int count= options.size();
+//		int size=0;
+//		String element = "india";
+//		for(int i=1; i<count; i++)
+//		{
+//			String text= options.get(i).getText();
+//			if(text.equalsIgnoreCase(element)) {
+//				//System.out.println(element+ " : is present");
+//				size++;
+//				break;
+//			}
+//		}
+//		if(size==1)
+//		{
+//			Reporter.log(element+ " : present", true);
+//		}
+//		else {
+//			Reporter.log(element+ " : not present", true);
+//		}
 		driver.quit();
 	}
 }
